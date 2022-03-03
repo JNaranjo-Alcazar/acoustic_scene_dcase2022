5c7f9ad06f9605d2e4fbd6cf574d83f8f0e33fdb
jnaranjo <jnaranjo@iti.es>
2022-03-03 16:04:11 +0100

    adding augmented conv tf

---
+++ src/models/aug_conv/aug_conv_tf.py
@@ -0,0 +1,62 @@
+'''
+Augmented conv
+'''
+
+from tensorflow.keras.layers import (Conv2D, MaxPool2D, Dropout, Input, BatchNormalization,
+                                    GlobalAveragePooling2D, GlobalMaxPooling2D, Dense, ELU)
+from tensorflow.keras.models import Model
+
+from attn_augconv import augmented_conv2d
+
+def construct_aug_conv_model(include_classification=True, nclasses=10, **parameters)
+
+    spectrogram_dim = parameters['spectrogram_dim']
+    filters = parameters['nfilters']
+    top_flatten = parameters['top_flatten']
+    dropout = parameters['dropout']
+    pooling = parameters['pooling']
+    kernel_size  = parameters['kernel_size']
+    verbose = parameters['verbose']
+
+    inp = Input(shape=spectrogram_dim)
+
+    for i in range(0, len(filters)):
+        if i == 0:
+            x = augmented_conv2d(inp, filters[i], kernel_size)
+        else:
+            x = augmented_conv2d(x, filters[i], kernel_size)
+        
+        x = BatchNormalization()(x)
+        x = ELU()(x)
+ 
+        x = MaxPool2D(pool_size=pooling[i])(x)
+        x = Dropout(rate=dropout[i])(x)
+
+    if top_flatten == 'avg':
+         x = GlobalAveragePooling2D()(x)
+    elif top_flatten == 'max':
+         x = GlobalMaxPooling2D()(x)
+
+    x = Dense(units=n_classes, activation='softmax', name='pred_layer')(x)
+
+    model = Model(inputs=inp, outputs=x)
+
+    if verbose:
+        print(model.summary())
+
+    return model
+
+
+if __name__ == '__main__':
+
+    audio_network_settings = {
+        'kernel_size': 3,
+        'nfilters': (40, 40),
+        'pooling': [(1, 10), (1, 10)],
+        'dropout': [0.3, 0.3],
+        'top_flatten': 'avg',
+        'spectrogram_dim': (64, 500, 3),
+        'verbose': True
+    }
+
+    audio_model = construct_aug_conv_model(include_classification=True, **audio_network_settings)
