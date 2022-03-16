8599de278d434c7fd7e86a53afc79edb92143894
jnaranjo <jnaranjo@iti.es>
2022-03-16 10:10:10 +0100

    adding cqt trainable

---
+++ src/models/baseline/baseline_torch_cqt.py
@@ -0,0 +1,88 @@
+'''
+Implement baseline with trainable CQT in Torch
+https://colab.research.google.com/drive/1coH54dfjAOxEyOjJrqscQRyC0_lmF04s#scrollTo=VDPrMNnoyQX_
+'''
+
+
+import torch
+from torch.nn import (Linear, ReLU, ELU, 
+                     Conv2d, MaxPool2d, Module, ModuleList, 
+                     BatchNorm2d, Dropout)
+import torch.nn.functional as F
+
+from nnAudio import Spectrogram
+
+from torchsummary import summary
+
+class Baseline(Module):
+    def __init__(self, nclasses, **audio_network_settings):
+        super(Baseline, self).__init__()
+
+        self.filters = audio_network_settings['nfilters']
+        self.dropout = audio_network_settings['dropout']
+        self.pooling = audio_network_settings['pooling']
+        self.kernel_size  = audio_network_settings['kernel_size']
+        self.verbose = audio_network_settings['verbose']
+        self.top_flatten = audio_network_settings['top_flatten']
+        
+        self.conv_layers = ModuleList()
+        # Adding Mel trainable layer
+        self.conv_layers.append(Spectrogram.CQT1992v2(sr=44100, hop_length=512, fmin=200,
+                                n_bins=82, bins_per_octave=12,
+                                norm=1, window='hann',
+                                center=True, pad_mode='constant', trainable=True))
+
+        for idx, nfilter in enumerate(self.filters):
+            if idx == 0:
+                self.conv_layers.append(Conv2d(1, nfilter, kernel_size=self.kernel_size, 
+                                        stride=1, padding=1))
+            else:
+                self.conv_layers.append(Conv2d(self.filters[idx-1], nfilter, kernel_size=self.kernel_size, 
+                                        stride=1, padding=1))
+
+            self.conv_layers.append(BatchNorm2d(nfilter))
+            self.conv_layers.append(ELU(inplace=False))
+
+            self.conv_layers.append(Conv2d(nfilter, nfilter, kernel_size=self.kernel_size, 
+                                        stride=1, padding=1))
+            self.conv_layers.append(BatchNorm2d(nfilter))
+            self.conv_layers.append(ELU(inplace=False))
+
+            self.conv_layers.append(MaxPool2d(kernel_size=self.pooling[idx], stride=self.pooling[idx]))
+            self.conv_layers.append(Dropout(p=self.dropout[idx], inplace=False))
+
+        self.classifier = Linear(nfilter, nclasses)
+
+    def forward(self, x):
+
+        i = 0
+        for layer in self.conv_layers:
+            if i == 1:
+                x = layer(x.unsqueeze(1))
+            else:
+                x = layer(x)
+            i += 1
+        if self.top_flatten == 'avg':
+            x = torch.mean(x.view(x.size(0), x.size(1), -1), dim=2)
+        out = F.softmax(self.classifier(x))
+
+        return out
+
+
+if __name__ == '__main__':
+
+    audio_network_settings = {
+        'kernel_size': 3,
+        'nfilters': (40, 40),
+        'pooling': [(1, 10), (1, 10)],
+        'dropout': [0.3, 0.3],
+        'top_flatten': 'avg',
+        #'spectrogram_dim': (64, 500, 1),
+        'verbose': True
+    }
+
+    model = Baseline(10, **audio_network_settings)
+
+    print(model)
+    #x = model.forward(torch.zeros(1, 441000))
+    summary(model, (1, 441000))
