8c139a12e1e6b0dc5dd9396150050a856da45ac7
jnaranjo <jnaranjo@iti.es>
2022-03-22 11:28:26 +0100

    adding parse args to train torch

--- src/models/train_model_torch.py
+++ src/models/train_model_torch.py
@@ -2,8 +2,8 @@
 Script to train model
 '''
 
-from gc import callbacks
-import numpy as np
+import argparse
+
 import torch
 
 import pytorch_lightning as pl
@@ -23,9 +23,38 @@ def get_dummy_dataset(in_features=44100, n_classes=10, batch_size=1000):
 
     return X,Y, X_val, Y_val
 
-# Read yaml
+def parse_args():
+    
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Argument for differents types of inference',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--train_features', required=True, type=str,
+        help="path to train features")
+    parser.add_argument(
+        '--train_labels', required=True, type=str,
+        help="path to train labels")
+    parser.add_argument(
+        '--validation_features', required=True, type=str,
+        help="path to validation features")
+    parser.add_argument(
+        '--validation_labels', required=True, type=str,
+        help="path to validation labels")
+    parser.add_argument(
+        '--colab', default=False, type=bool, 
+        help="path to setup GPU")
+    args = parser.parse_args()
+    return args
 
-audio_network_settings = {
+
+
+
+if __name__ == '__main__':
+    
+    # Read yaml
+    # Import model as yaml
+    
+    audio_network_settings = {
         'kernel_size': 3,
         'nfilters': (40, 40),
         'pooling': [(1, 2), (1, 2)],
@@ -34,27 +63,42 @@ audio_network_settings = {
         'spectrogram_dim': (64, 500, 1),
         'verbose': True
     }
+    
+    opt = parse_args()
+    
+    print(f"training features path: {opt.train_features}")
+    print(f"training labels path: {opt.train_labels}")
+    print(f"validation features path: {opt.val_features}")
+    print(f"validation labels path: {opt.val_labels}")
+    print(f"Set in colab: {opt.colab}")
+    
+    X = torch.load(opt.train_features)
+    Y = torch.load(opt.train_labels)
+    X_val = torch.load(opt.val_features)
+    Y_val = torch.load(opt.val_labels)
 
-# Import model as yaml
+    model = Baseline(10, **audio_network_settings)
+    #summary(model, (1, 44100))
 
-model = Baseline(10, **audio_network_settings)
-summary(model, (1, 44100))
+    logger = CSVLogger("lightning_logs", name="my_exp_name")
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    # Train
+    if opt.colab is False:
+        trainer = pl.Trainer(progress_bar_refresh_rate=20, max_epochs=5, 
+                            logger=logger, callbacks=[lr_monitor]) #log_every_n_steps=int(n_total_samples/batch_size)
+    elif opt.colab:
+        trainer = pl.Trainer(progress_bar_refresh_rate=20, max_epochs=500, 
+                            logger=logger, callbacks=[lr_monitor], gpus=1)
+    #trainer = pl.Trainer(progress_bar_refresh_rate=20, max_epochs=500, gpus=1)
 
-logger = CSVLogger("lightning_logs", name="my_exp_name")
-lr_monitor = LearningRateMonitor(logging_interval='epoch')
-# Train
-trainer = pl.Trainer(progress_bar_refresh_rate=20, max_epochs=5, 
-                     logger=logger, callbacks=[lr_monitor]) #log_every_n_steps=int(n_total_samples/batch_size) 
-#trainer = pl.Trainer(progress_bar_refresh_rate=20, max_epochs=500, gpus=1)
-
-X, Y, X_val, Y_val = get_dummy_dataset()
-train_data = torch.utils.data.TensorDataset(torch.tensor(X), torch.tensor(Y, dtype=torch.long))
-val_data = torch.utils.data.TensorDataset(torch.tensor(X_val), torch.tensor(Y_val, dtype=torch.long))
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=16)
-val_loader = torch.utils.data.DataLoader(val_data, batch_size=16)
+    #X, Y, X_val, Y_val = get_dummy_dataset()
+    train_data = torch.utils.data.TensorDataset(torch.tensor(X), torch.tensor(Y, dtype=torch.long))
+    val_data = torch.utils.data.TensorDataset(torch.tensor(X_val), torch.tensor(Y_val, dtype=torch.long))
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=32)
 
 
-# Need to prepare acc and on epoch end, pbar -> video
-trainer.fit(model, train_loader, val_loader)
+    # Need to prepare acc and on epoch end, pbar -> video
+    trainer.fit(model, train_loader, val_loader)
 
-# Finish training -> prints, logs, etc
+    # Finish training -> prints, logs, etc
