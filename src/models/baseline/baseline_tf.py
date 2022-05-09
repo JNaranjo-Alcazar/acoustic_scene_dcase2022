0996d887022690c63ca753687782f6cc5fd010e0
jnaranjo <jnaranjo@iti.es>
2022-05-09 16:29:40 +0200

    removing second layer, more macs

--- src/models/baseline/baseline_tf.py
+++ src/models/baseline/baseline_tf.py
@@ -27,9 +27,11 @@ def construct_baseline_model(include_classification=True, nclasses=10, **paramet
         x = BatchNormalization()(x)
         x = ELU()(x)
  
-        x = Conv2D(filters[i], kernel_size, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = ELU()(x)
+        if i!=0:
+
+            x = Conv2D(filters[i], kernel_size, padding='same')(x)
+            x = BatchNormalization()(x)
+            x = ELU()(x)
 
         x = MaxPool2D(pool_size=pooling[i])(x)
         x = Dropout(rate=dropout[i])(x)
